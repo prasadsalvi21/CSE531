b2b2ae6df7f5e1bf2d624c04e93af082252671ac
Prasad Salvi <prasadde@buffalo.edu>
2016-10-14 21:57:40 -0400

    Added graph vertices in Graph1.class

--- src/project1/Graph1.java
+++ src/project1/Graph1.java
@@ -6,13 +6,40 @@ public class Graph1 {
 		  int w;
 		  
 		  public Graph1 (int u, int v, int w) {
-		    u = this.u;
-		    v = this.v;
-		    w= this.w;
+		    this.u=u;
+		    this.v=v;
+		   this.w=w;
 		  }
 		  
 		  public String toString() {
 		    return "(" + u + ",w=" + w + ")";
 		  }
+
+	
+
+		public int getU() {
+			return u;
+		}
+
+		public void setU(int u) {
+			this.u = u;
+		}
+
+		public int getV() {
+			return v;
+		}
+
+		public void setV(int v) {
+			this.v = v;
+		}
+
+		public int getW() {
+			return w;
+		}
+
+		public void setW(int w) {
+			this.w = w;
+		}
+		
 		}
 
--- src/project1/PrimsAlgo.java
+++ src/project1/PrimsAlgo.java
@@ -8,18 +8,19 @@ public class PrimsAlgo
 {
 	public static void main(String[] args)
 	{
-		int n,m,u,v,i=0,w=0;
-		ArrayList<Graph1>[] g;
+		int n=0,m=0,u,v,i=0,w,k=0;
+		
 		int nl=0;
 		try
 		{
 			//BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
-			Path filePath = Paths.get("test5.txt");
+			Path filePath = Paths.get("test1.txt");
 			Scanner src = new Scanner(filePath);
 			List<Integer> integers = new ArrayList<>();
 			n=src.nextInt();
 			m=src.nextInt();
-			g = new ArrayList[n];
+			ArrayList<Graph1> g;
+			g=new ArrayList<Graph1>(m);
 			if(n>10000||m>10000||n<1||m<1)
 			{
 				System.out.println("Invalid graph entries present in input file");
@@ -27,7 +28,7 @@ public class PrimsAlgo
 			else
 			{
 			System.out.println("n="+n+" m="+m);
-			while (src.hasNext()&&(i<=n)) 
+			while (src.hasNext()&&(i<m)) 
 			{
 				if (src.hasNextInt()) 
 			    {
@@ -51,11 +52,8 @@ public class PrimsAlgo
 						break;
 					}
 					System.out.print(" w="+w);
-					g[i].add(new Graph1(u,v,w));
-					i++;
-					
-					System.out.println();
-					
+					g.add(new Graph1(u,v,w));
+					System.out.println("");
 				}
 				else
 			        src.next();
@@ -65,6 +63,17 @@ public class PrimsAlgo
 				{
 				System.out.println("The number of entries for edges in file "+nl+" doesnot match with m="+m+" specified");
 				}
+				for (i = 0; i < g.size(); i++) {
+					System.out.print("U="+g.get(i).getU());
+					System.out.print(" V="+g.get(i).getV());
+					System.out.print(" W="+g.get(i).getW());
+					System.out.println("");
+				}
+				u=g.get(0).getU();
+				v=g.get(0).getV();
+				w=g.get(0).getW();
+				
+				
 			}
 		}
 		catch(Exception e)
