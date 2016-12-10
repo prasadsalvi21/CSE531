fd370c889bdc81cae3bdcd1571a7380b785fba59
Prasad Salvi <prasadde@buffalo.edu>
2016-12-10 09:21:20 -0500

    Balanced string length

--- src/BalancedString2.java
+++ src/BalancedString2.java
@@ -1,35 +1,28 @@
+import java.util.Stack;
 
 public class BalancedString2 {
 
 	public static void main(String[] args) {
-		String s="((())()())";
-		char c[]=s.toCharArray();
-		char count[]=new char[s.length()+1];
-		int top=0;
-		int longest=0;
-		boolean flag=true;
-		for(int i=0;i<s.length();i++)
-		{
-			if(c[i]==')')
-			{
-				top--;
-				if(top<0)
-				{
-					longest=0;
-					top=0;
-				}
-				longest++;
-			}
-			else if(c[i]=='(')
-			{
-				top++;
-				count[top]='(';
-				flag=true;
-				longest++;
-			}
-				
-		}
-			System.out.println("longest sequence = "+longest);
+		String s="())";
+		 Stack<Integer> stack = new Stack<Integer>();
+		    int max=0;
+		    int top = -1;
+		    for(int i=0;i<s.length();i++){
+		        if(s.charAt(i)=='(') stack.push(i);            
+		        else {
+		            if (stack.isEmpty())
+		            	top=i;
+		            else{
+		                stack.pop();
+		                if(stack.isEmpty())
+		                	max=Math.max(max,i-top);
+		                else 
+		                	max=Math.max(max,i-stack.peek());
+		               }
+		            }
+		        }
+		    System.out.println("Length of balanced String = "+max);
+		
 
 	}
 
